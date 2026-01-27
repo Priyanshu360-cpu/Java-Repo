c7d24d03c457b4b0ba34331e122a930b96f64b7d
Priyanshu360-cpu <tpriyanshu775@gmail.com>
2026-01-27 15:50:17 +0530

    added collections with arralyist

--- generics/src/main/java/com/ibm/generics/Application.java
+++ generics/src/main/java/com/ibm/generics/Application.java
@@ -1,5 +1,9 @@
 package com.ibm.generics;
-class Point<T>{
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+class Point<T extends Number>{
 	private T x;
 	private T y;
 	Point(T x,T y){
@@ -19,5 +23,12 @@ public class Application
 	{
 		Point<Integer> point = new Point<Integer>(2,3);
 		System.out.println(point.getx());
+		ArrayList<String> arrayList=new ArrayList<String>();
+		arrayList.add("hi");
+		arrayList.add("how are you?");
+		Iterator<String> iterator = arrayList.iterator();
+		while(iterator.hasNext()) {
+			System.out.println(iterator.next());
+		}
 	}
 }
