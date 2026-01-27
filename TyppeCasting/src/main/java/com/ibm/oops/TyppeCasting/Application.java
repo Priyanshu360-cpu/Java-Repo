f735bdede2fd830cd5e27b8b6d53ffbdbec0bd3b
Priyanshu360-cpu <tpriyanshu775@gmail.com>
2026-01-27 14:46:23 +0530

    added super keyword reference

--- TyppeCasting/src/main/java/com/ibm/oops/TyppeCasting/Application.java
+++ TyppeCasting/src/main/java/com/ibm/oops/TyppeCasting/Application.java
@@ -5,11 +5,27 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.LinkedList;
 import java.util.List;
-
+class Base{
+	Base(int x){
+		System.out.println(x);
+	}
+	public void test(int y) {
+		System.out.println(y);
+	}
+}
+class Derived extends Base{
+	Derived(int x){
+		super(x);
+	}
+	public void test(int z) {
+		super.test(z);
+	}
+}
 public class Application
 {
 	public static void main( String[] args )
 	{
+		Derived derived=new Derived(2);
 		float f=(float) 10.2;
 		Calendar calendar = new GregorianCalendar();//upscale casting
 		System.out.println(calendar.get(Calendar.DAY_OF_MONTH));
