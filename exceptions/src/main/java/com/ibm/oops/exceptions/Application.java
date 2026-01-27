d20118420db8959019497df12d203b03ec6e35b1
Priyanshu360-cpu <tpriyanshu775@gmail.com>
2026-01-27 11:24:25 +0530

    manual exception handling and creation

--- exceptions/src/main/java/com/ibm/oops/exceptions/Application.java
+++ exceptions/src/main/java/com/ibm/oops/exceptions/Application.java
@@ -1,9 +1,30 @@
 package com.ibm.oops.exceptions;
-
+class IllegalAgeException extends Exception{
+	@Override
+	public String getMessage() {
+		return "Not allowed for voting";
+	}
+}
+class Voting{
+	public void isallowed(int age)throws IllegalAgeException {
+		if(age<18) {
+			throw new IllegalAgeException();
+		}
+	}
+}
 public class Application
 {
 	public static void main( String[] args )
 	{
+	Voting ob=new Voting();
+	try {
+		ob.isallowed(18);
+	}catch(IllegalAgeException exception) {
+		exception.printStackTrace();
+		System.out.println("Not allowed to vote");
+	}finally {
+		System.out.println("All cleared");
+	}
 		try {
 	System.out.println(Integer.parseInt(args[0])/Integer.parseInt(args[1]));	
 		}catch(ArrayIndexOutOfBoundsException | NumberFormatException exception) {
