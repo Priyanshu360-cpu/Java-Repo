45d8ea854f4576d6fe5ec9bc3ff8f471ac149747
Priyanshu360-cpu <tpriyanshu775@gmail.com>
2026-01-28 15:18:26 +0530

    Streams and lambda

--- lambdas/src/main/java/com/ibm/lambdas/Application.java
+++ lambdas/src/main/java/com/ibm/lambdas/Application.java
@@ -1,4 +1,8 @@
 package com.ibm.lambdas;
+
+import java.util.Arrays;
+import java.util.List;
+
 interface Printer{
 	public void printing(String printerName);
 }
@@ -6,6 +10,16 @@ class Process{
 	public void execute(String printerName,Printer printer) {
 	printer.printing(printerName);
 	}
+};
+class Stream{
+	public static void streams() { 
+	List<Integer> arr1=Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+	arr1.forEach((number)->System.out.println(number));
+	arr1.forEach(System.out::print);
+	arr1.stream().map(element->String.valueOf(element)).forEach(System.out::println);
+	System.out.println(arr1.stream().reduce(0,(total,number)->Integer.sum(total,number)));
+	}
+	
 }
 public class Application
 {
@@ -24,7 +38,7 @@ public class Application
         process.execute("Windows", Application::printIt);
 		process.execute("HP",(printerName)->System.out.println(printerName+" Printing"));
 		process.execute("Cannon",(printerName)->System.out.println(printerName+" Printing"));
-
+Stream.streams();
 	}
 	static void printIt(String name) {
 		System.out.println(name);
