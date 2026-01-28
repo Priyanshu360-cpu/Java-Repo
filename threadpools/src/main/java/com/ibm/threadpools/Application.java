d89a56505d2e6b94a5762fd196e87f997ee00379
Priyanshu360-cpu <tpriyanshu775@gmail.com>
2026-01-28 11:35:18 +0530

    execution service

---
+++ threadpools/src/main/java/com/ibm/threadpools/Application.java
@@ -0,0 +1,28 @@
+package com.ibm.threadpools;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+class Test implements Runnable{
+	int count;
+	Test(int counter){
+	  count=counter;
+	}
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		System.out.println(Thread.currentThread()+"---->"+this.count);
+	}
+	
+}
+public class Application
+{
+	public static void main( String[] args )
+	{
+		ExecutorService service=Executors.newFixedThreadPool(1000000000);
+		for(int counter =1;counter<=10;counter++) {
+			service.submit(new Test(counter));
+		}
+		service.shutdown();
+	}
+}
